1d15d9bfb450f8857fcd5f79701ea2c0bee6fb9e
ts-geek43 <manish.prajapati@techstaunch.com>
2023-04-20 13:04:28 +0530

    watch flag added in codegen config file

--- codegen.ts
+++ codegen.ts
@@ -4,6 +4,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: "https://api.cartql.com/",
+  watch:true,
   generates: {
     "generated/graphql": {
       plugins: ["typescript", "typescript-resolvers"]
